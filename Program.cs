d736fdb869ecc80f8a2cf51897933a7b27073930
Phantom30 <71391909+Phantom30@users.noreply.github.com>
2021-12-14 15:09:48 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ThisIsADartGame_v01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello ould!");
+        }
+    }
+}
