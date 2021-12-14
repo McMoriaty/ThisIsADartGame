29dde21a3460563a31bfe640fc2b748ba09342cf
Phantom30 <71391909+Phantom30@users.noreply.github.com>
2021-12-14 16:00:30 +0100

    New Commit

--- Program.cs
+++ Program.cs
@@ -2,86 +2,103 @@
 
 namespace ThisIsADartGame_v01
 {
-    
-    
-        class Program
+    class Program
+    {
+        static void ConventMultiToRadius(int Radius)
         {
-            static void ConventMultiToRadius(int Radius)
+            int[] Feldpositionen = new int[6] { 5, 15, 50, 60, 80, 90 };
+
+
+            for (int i = 0; i < 5; i++)
             {
-                int[] Feldpositionen = new int[6] { 5, 15, 50, 60, 80, 90 };
-
-
-                for (int i = 0; i < 5; i++)
+                if (Radius < Feldpositionen[i])
                 {
-                    if (Radius < Feldpositionen[i])
-                    {
-                        Console.WriteLine(2);
-                    }
-                    else
-                    {
-                        Console.WriteLine(1);
-                    }
+                    Console.WriteLine(2);
+                }
+                else
+                {
+                    Console.WriteLine(1);
                 }
             }
-            static void EingabeZuDaten(int Radius, string Multi)
+        }
+        static void EingabeZuDaten(int ZielZahl,int Radius, string Multi)
+        {
+            int[] Feld = new int[20] { 6, 13, 4, 18, 1, 20, 5, 12, 9, 14, 11, 8, 16, 7, 19, 3, 17, 2, 15, 10 };
+
+            // Diese For-Schleife findet die genaue Poitsition des angestrebten Feldes in der Variable Feld heraus.
+            for (int i = 0; i < Feld.Length; i++)
             {
-                int[] Feld = new int[20] { 6, 13...};
+                if (Feld[i] == ZielZahl)
+                {
+                    Console.WriteLine(i+1);
+                    i = 20;
+                }
             }
 
-            static void Main(string[] args)
+        }
+
+        static void Main(string[] args)
+        {
+            //Abfrage über den Bereich den der Benutzer treffen will.
+            Console.WriteLine("Für einen Wurf in das Bulseye drücken sie die 1.");
+            Console.WriteLine("Für einen Wurf in das Eye drücken sie die 2.");
+            Console.WriteLine("Für einen Wurf in ein Triple Wert drücken sie die 3.");
+            Console.WriteLine("Für einen Wurf in das Doubble Wert drücken sie die 4.");
+            Console.WriteLine("Für einen Wurf in das Normalen drücken sie die 5.");
+
+            int b = Int32.Parse(Console.ReadLine());
+            int Radius = 0;
+            int Winkel = 0;
+            string Multi = "";
+            int ZielZahl = 0;
+
+
+            if (b == 1)
             {
-                Console.WriteLine("Für einen Wurf in das Bulseye drücken sie die 1.");
-                Console.WriteLine("Für einen Wurf in das Eye drücken sie die 2.");
-                Console.WriteLine("Für einen Wurf in ein Triple Wert drücken sie die 3.");
-                Console.WriteLine("Für einen Wurf in das Doubble Wert drücken sie die 4.");
-                Console.WriteLine("Für einen Wurf in das Normalen drücken sie die 5.");
+                Radius = 0;
+                Winkel = 0;
+            }
+            else if (b == 2)
+            {
+                Radius = 0;
 
-                int b = Int32.Parse(Console.ReadLine());
-                int Radius = 0;
-                int Winkel = 0;
-
-
-                if (b == 1)
+            }
+            else
+            {
+                if (b == 3)
                 {
-                    Radius = 0;
-                    Winkel = 0;
-                }
-                else if (b == 2)
-                {
-                    Radius = 0;
-
-                }
-                else if (b == 3)
-                {
-                    string Multi = "Triple";
+                    Multi = "Triple";
 
                     Console.WriteLine("Geben sie die Zahl zwischen 1-20 ein welche sie treffen wollen!");
-                    int ZielZahl = Int32.Parse(Console.ReadLine());
+                    ZielZahl = Int32.Parse(Console.ReadLine());
 
                 }
                 else if (b == 4)
                 {
-                    string Multi = "Doubble";
+                    Multi = "Doubble";
 
                     Console.WriteLine("Geben sie die Zahl zwischen 1-20 ein welche sie treffen wollen!");
-                    int ZielZahl = Int32.Parse(Console.ReadLine());
+                    ZielZahl = Int32.Parse(Console.ReadLine());
 
                 }
                 else if (b == 5)
                 {
-                    string Multi = "Singel";
+                    Multi = "Singel";
 
                     Console.WriteLine("Geben sie die Zahl zwischen 1-20 ein welche sie treffen wollen!");
-                    int ZielZahl = Int32.Parse(Console.ReadLine());
+                    ZielZahl = Int32.Parse(Console.ReadLine());
 
                 }
                 else
                 {
                     Console.WriteLine("Möchten sie mich verarschen.");
                 }
-
-
-                ConventMultiToRadius(Radius);
             }
+
+
+            ConventMultiToRadius(Radius);
+            EingabeZuDaten(ZielZahl,Radius, Multi);
+
         }
     }
+}
