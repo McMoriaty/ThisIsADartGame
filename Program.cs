da0012cc2d9d35499f047211f84bbaa0396a9d82
Phantom30 <71391909+Phantom30@users.noreply.github.com>
2022-01-04 22:01:42 +0100

    Test file

--- Program.cs
+++ Program.cs
@@ -1,51 +1,53 @@
 ﻿using System;
 
-namespace ThisIsADartGame_v01
+namespace DartGame
 {
     class Program
     {
-        static void ConventMultiToRadius(int Radius)
+        private int ZielZahlInWinkel(int Zielzahl)
         {
-            int[] Feldpositionen = new int[6] { 5, 15, 50, 60, 80, 90 };
-
-
-            for (int i = 0; i < 5; i++)
+            int[] ScheibenZahl = new int[20] { 6, 13, 4, 18, 1, 20, 5, 12, 9, 14, 11, 8, 16, 7, 19, 3, 17, 2, 15, 10 };
+            int[] Winkelmitte = new int[20] { 0, 18, 36, 54, 72, 90, 108, 126, 144, 162, 180, 198, 216, 234, 252, 270, 288, 306, 324, 342 };
+            int aby;
+            for (int i = 0; i < 19; i++)
             {
-                if (Radius < Feldpositionen[i])
+                if (Zielzahl == ScheibenZahl[i])
                 {
-                    Console.WriteLine(2);
-                }
-                else
-                {
-                    Console.WriteLine(1);
+
+
+
+                    i = aby;
+
+
                 }
             }
+            return aby;
         }
-        static void EingabeZuDaten(int ZielZahl,int Radius, string Multi)
+
+
+        private static Array Genauigkeit(int Radius, int Winkel, int Promille)
         {
-            int[] Feld = new int[20] { 6, 13, 4, 18, 1, 20, 5, 12, 9, 14, 11, 8, 16, 7, 19, 3, 17, 2, 15, 10 };
-            int x = 0;
-            int y = 0;
+            Random WinkelKorrektur = new Random();
+            int WinkelKorrekturAnwendung = WinkelKorrektur.Next(0, 15);
+            Winkel = Winkel + WinkelKorrekturAnwendung + WinkelKorrekturAnwendung * Promille;
 
-            // Diese For-Schleife findet die genaue Poitsition des angestrebten Feldes in der Variable Feld heraus.
-            for (int i = 0; i < Feld.Length; i++)
-            {
-                if (Feld[i] == ZielZahl)
-                {
-                    Console.WriteLine(i+1);
-                    x = i + 1;
-                    i = 20;
-                }
-            }
 
-            // Hier erhält man den Winkel des Feldes.
-            y = x * 18 - 8;
 
+            Random RadiusKorrektur = new Random();
+            int RadiusKorrekturAnwendung = RadiusKorrektur.Next(0, 20);
+            Radius = Radius + RadiusKorrekturAnwendung + RadiusKorrekturAnwendung * Promille;
+
+
+
+            int[] WinkelRadius = new int[2] { Winkel, Radius };
+            Console.WriteLine(Radius);
+            Console.WriteLine(Winkel);
+            return WinkelRadius;
         }
 
+        
         static void Main(string[] args)
         {
-            //Abfrage über den Bereich den der Benutzer treffen will.
             Console.WriteLine("Für einen Wurf in das Bulseye drücken sie die 1.");
             Console.WriteLine("Für einen Wurf in das Eye drücken sie die 2.");
             Console.WriteLine("Für einen Wurf in ein Triple Wert drücken sie die 3.");
@@ -53,11 +55,11 @@ namespace ThisIsADartGame_v01
             Console.WriteLine("Für einen Wurf in das Normalen drücken sie die 5.");
 
             int b = Int32.Parse(Console.ReadLine());
+            
+            string Multi;
             int Radius = 0;
             int Winkel = 0;
-            string Multi = "";
-            int ZielZahl = 0;
-
+            int Promille;
 
             if (b == 1)
             {
@@ -66,43 +68,53 @@ namespace ThisIsADartGame_v01
             }
             else if (b == 2)
             {
-                Radius = 0;
+                Radius = 10;
+
+            }
+            else if (b == 3)
+            {
+                Console.WriteLine("Geben sie die Zahl zwischen 1-20 ein welche sie treffen wollen!");
+                int ZielZahl = Int32.Parse(Console.ReadLine());
+                Radius = 55;
+
+            }
+            else if (b == 4)
+            {
+                Console.WriteLine("Geben sie die Zahl zwischen 1-20 ein welche sie treffen wollen!");
+                int ZielZahl = Int32.Parse(Console.ReadLine());
+
+                Radius = 85;
+            }
+            else if (b == 5)
+            {
+                Console.WriteLine("Geben sie die Zahl zwischen 1-20 ein welche sie treffen wollen!");
+                int ZielZahl = Int32.Parse(Console.ReadLine());
+
+                Console.WriteLine("Möchten Sie in das grössere/äussere Singel Feld schiessen, dann drücken sie 1. Wenn sie jedoch das kleinere/innere treffen wollen drücken Sie 2.");
+                int GrossKleinFeld = Int32.Parse(Console.ReadLine());
+                if (GrossKleinFeld == 1)
+                {
+                    Radius = 70;
+                }
+                else if (GrossKleinFeld == 2)
+                {
+                    Radius = 38;
+                }
+
+
+                Console.WriteLine("Wie viel Shots haben Sie intus");
+                Promille = Int32.Parse(Console.ReadLine());
+                Console.WriteLine(Genauigkeit(Radius, Winkel, Promille));
 
             }
             else
             {
-                if (b == 3)
-                {
-                    Multi = "Triple";
-
-                    Console.WriteLine("Geben sie die Zahl zwischen 1-20 ein welche sie treffen wollen!");
-                    ZielZahl = Int32.Parse(Console.ReadLine());
-
-                }
-                else if (b == 4)
-                {
-                    Multi = "Doubble";
-
-                    Console.WriteLine("Geben sie die Zahl zwischen 1-20 ein welche sie treffen wollen!");
-                    ZielZahl = Int32.Parse(Console.ReadLine());
-
-                }
-                else if (b == 5)
-                {
-                    Multi = "Singel";
-
-                    Console.WriteLine("Geben sie die Zahl zwischen 1-20 ein welche sie treffen wollen!");
-                    ZielZahl = Int32.Parse(Console.ReadLine());
-
-                }
-                else
-                {
-                    Console.WriteLine("Möchten sie mich verarschen.");
-                }
+                Console.WriteLine("Möchten sie mich verarschen.");
             }
 
-            ConventMultiToRadius(Radius);
-            EingabeZuDaten(ZielZahl,Radius, Multi);
+
+
+
 
         }
     }
---
+++ Test/Test_functionpolarcart/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Test_functionpolarcart
+{
+    class Program
+    {
+        private static Tuple<double,double> CartToPolar(double x, double y)
+        {
+            double radius = Math.Sqrt((x * x) + (y * y));
+            double alpha = Math.Atan(y/x);
+
+            try
+            {
+                return new Tuple<double, double>(radius, alpha);
+            }
+            catch (Exception)
+            {
+                return new Tuple<double, double>(radius, alpha);
+            }
+
+            
+        }
+        private static Tuple<double, double> PolarToCart(double radius, double alpha)
+        {
+            double x = radius * Math.Cos(alpha);
+            double y = radius * Math.Sin(alpha);
+
+            try
+            {
+                return new Tuple<double, double>(x, y);
+            }
+            catch (Exception)
+            {
+                return new Tuple<double, double>(x, y);
+            }
+
+
+        }
+        static void Main(string[] args)
+        {
+            double x;
+            double y;
+
+            Console.WriteLine(CartToPolar(1, 3));
+            Console.WriteLine(PolarToCart(3.1622776601683795, 1.2490457723982544));
+        }
+    }
+}
