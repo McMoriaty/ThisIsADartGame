52384acbdd4db9b8a9c7ba67004a3ae311333100
Phantom30 <71391909+Phantom30@users.noreply.github.com>
2022-01-14 23:52:07 +0100

    Final commit

--- Program.cs
+++ Program.cs
@@ -2,48 +2,117 @@
 
 namespace DartGame
 {
-    public class DartGame 
-    {
-        public override string Name => "Dart";
-        public override string Description => "In diesem Spiel geht es um Dart. Sie dürfen einen Pfeil werfen und je nach Anzahl Punkten wird Ihnen Punkte vom Startwert abgezogen.  Ziel dieses Spiel ist es 0 Punkte zu erreichen. Wenn Sie 0 Punkte unterschreiten bekommen sie eine kleine Strafe und das Spiel ist beendet. Je weniger Würfe sie gebraucht haben, desto besser haben Sie abgeschnitten. ";
-        public override string Rules => "Sie werfen einen Pfeil auf ein bestimmtes Feld, mit gewisser Wahrscheinlichkeit treffen sie genau dieses Feld oder in einem Kreisförmigen Umfeld ein anderes Feld. Diese Punkte werden Ihnen vom Startwert (501 Punkte) abgezogen. Sie spielen solange bis Sie einen Wert erreichen der kleiner  oder gleich 0 ist. Wenn sie es schaffen 0 Punkte genau zu erreichen ist Ihr Endergebnis gleich der Anzahl Pfeile die Sie geworfen haben, und das Spiel ist beendet. Wenn sie 0 Punkte unterschreiten bekommen Sie 2 mal die Punkte welche Sie unter 0 geworfen haben zu den Anzahl Würfen dazu addiert, und somit ist das Spiel auch hier beendet.";
-        public override bool TheHigherTheBetter => false;
-        public override int LevelMax => 1;
-    }
+    //public class DartGame 
+    //{
+    //    public override string Name => "Dart";
+    //    public override string Description => "In diesem Spiel geht es um Dart. Sie dürfen einen Pfeil werfen und je nach Anzahl Punkten wird Ihnen Punkte vom Startwert abgezogen.  Ziel dieses Spiel ist es 0 Punkte zu erreichen. Wenn Sie 0 Punkte unterschreiten bekommen sie eine kleine Strafe und das Spiel ist beendet. Je weniger Würfe sie gebraucht haben, desto besser haben Sie abgeschnitten. ";
+    //    public override string Rules => "Sie werfen einen Pfeil auf ein bestimmtes Feld, mit gewisser Wahrscheinlichkeit treffen sie genau dieses Feld oder in einem Kreisförmigen Umfeld ein anderes Feld. Diese Punkte werden Ihnen vom Startwert (501 Punkte) abgezogen. Sie spielen solange bis Sie einen Wert erreichen der kleiner  oder gleich 0 ist. Wenn sie es schaffen 0 Punkte genau zu erreichen ist Ihr Endergebnis gleich der Anzahl Pfeile die Sie geworfen haben, und das Spiel ist beendet. Wenn sie 0 Punkte unterschreiten bekommen Sie 2 mal die Punkte welche Sie unter 0 geworfen haben zu den Anzahl Würfen dazu addiert, und somit ist das Spiel auch hier beendet.";
+    //    public override bool TheHigherTheBetter => false;
+    //    public override int LevelMax => 1;
+    //}
     class Program
     {
-        private static int alpha(int Number)
-        {
-            int[] TargetNumber = new int[20] { 6, 13, 4, 18, 1, 20, 5, 12, 9, 14, 11, 8, 16, 7, 19, 3, 17, 2, 15, 10 };
-            int[] middlealpha = new int[20] { 0, 18, 36, 54, 72, 90, 108, 126, 144, 162, 180, 198, 216, 234, 252, 270, 288, 306, 324, 342 };
-
-            for (int i = 0; i < 19; i++)
-            {
-                if (Number == TargetNumber[i])
-                {
-                    return middlealpha[i];
-                }
-            }
-            return 0;
-        }
-        private static Array CartToPolar(double x, double y)
+        private static double[] CartToPolar(double x, double y)
         {
             double radius = Math.Sqrt((x * x) + (y * y));
-            double alpha = Math.Atan(y / x);
+            double alpha;
+
+            if (x > 0 && y > 0)
+            {
+                alpha = Math.Atan(y / x) * 180 / Math.PI;
+            }
+            else if (x < 0 || y >= 0)
+            {
+                alpha = (Math.Atan(y / x) + Math.PI) * 180 / Math.PI;
+            }
+            else if (x < 0 && y < 0)
+            {
+                alpha = (Math.Atan(y / x) - Math.PI) * 180 / Math.PI;
+            }
+            else if (x > 0 && y < 0)
+            {
+                alpha = (Math.Atan(y / x) + 2 * Math.PI) * 180 / Math.PI;
+            }
+            else
+            {
+                alpha = 0;
+            }
 
             double[] CartToPolar = new double[2] { radius, alpha };
 
             return CartToPolar;
-
         }
-        private static Array PolarToCart(double radius, double alpha)
+
+        private static double[] PolarToCart(double radius, double alpha)
         {
-            double x = radius * Math.Cos(alpha);
-            double y = radius * Math.Sin(alpha);
+            double deg = Math.PI * alpha / 180;
+
+            double x = radius * Math.Cos(deg);
+            double y = radius * Math.Sin(deg);
 
             double[] PolarToCart = new double[2] { x, y };
 
             return PolarToCart;
+        }
+
+        private static void ArrayToString(double[] Target)
+        {
+            foreach (var item in Target)
+            {
+                Console.WriteLine(Convert.ToString(item));
+            }
+        }
+
+        private static int TargetNumberToAlpha(int number)
+        {
+            int[] TargetNumber = new int[20] { 6, 13, 4, 18, 1, 20, 5, 12, 9, 14, 11, 8, 16, 7, 19, 3, 17, 2, 15, 10 };
+            int[] MiddleAlpha = new int[20] { 0, 18, 36, 54, 72, 90, 108, 126, 144, 162, 180, 198, 216, 234, 252, 270, 288, 306, 324, 342 };
+
+            for (int i = 0; i < 19; i++)
+            {
+                if (number == TargetNumber[i])
+                {
+                    return MiddleAlpha[i];
+                }
+            }
+            return 0;
+        }
+        private static int AlphaToTargetNumber(double alpha)
+        {
+            int[] TargetNumber = new int[20] { 6, 13, 4, 18, 1, 20, 5, 12, 9, 14, 11, 8, 16, 7, 19, 3, 17, 2, 15, 10 };
+            int[] MiddleAlpha = new int[20] { 6, 18, 36, 54, 72, 90, 108, 126, 144, 162, 180, 198, 216, 234, 252, 270, 288, 306, 324, 342 };
+
+            for (int i = 0; i < 19; i++)
+            {
+                if (alpha <= MiddleAlpha[i])
+                {
+                    return TargetNumber[i];
+                }
+            }
+            return 0;
+        }
+
+        private static double[] play(double Radius, int Alpha)
+        {
+            Random random = new Random();
+            double RandomRadius = (double)random.NextDouble();
+            double RandomAlpha = (double)random.Next(0, 360);
+
+            double[] EndDistance = new double[2] { 0, 0 };
+            double[] EndResult= new double[2] { 0, 0 };
+            for (int i = 0; i < 2; i++)
+            {
+                EndDistance[i] = PolarToCart(RandomRadius, RandomAlpha)[i] + PolarToCart(Radius, TargetNumberToAlpha(Alpha))[i];
+            }
+
+            for (int i = 0; i < 2; i++)
+            {
+                EndResult[i] = CartToPolar(EndDistance[0], EndDistance[1])[i];
+            }
+            AlphaToTargetNumber(End)
+
+
+            return 0;
         }
 
         static void Main(string[] args)
@@ -59,44 +128,39 @@ namespace DartGame
                 Console.WriteLine("Für einen Wurf in das Doubble Wert drücken sie die 4.");
                 Console.WriteLine("Für einen Wurf in das Normalen drücken sie die 5.");
 
-                int b = Int32.Parse(Console.ReadLine());
+                int TargetRingNumber = Int32.Parse(Console.ReadLine());
 
-                string Multi;
-                int Radius = 0;
-                int Winkel = 0;
+                int Radius = 0; // Mid lenght from the center to the target field
+                int Alpha = 0;  // Mid degree of the target fiel
                 int Promille;
 
-                if (b == 1)
+                if (TargetRingNumber == 1)
                 {
                     Radius = 0;
-                    Winkel = 0;
+                    Alpha = 0;
                 }
-                else if (b == 2)
+                else if (TargetRingNumber == 2)
                 {
                     Radius = 10;
 
                 }
-                else if (b == 3)
+                else if (TargetRingNumber == 3)
                 {
                     Console.WriteLine("Geben sie die Zahl zwischen 1-20 ein welche sie treffen wollen!");
                     int Number = Int32.Parse(Console.ReadLine());
                     Radius = 55;
-                    Random random = new Random();
-                    double RandomRadius = (double)random.NextDouble();
-                    double RandomAlpha = (double)random.Next(0,360);
 
-                    Console.WriteLine(PolarToCart(Radius, alpha(Number));
-                    PolarToCart(RandomRadius,RandomAlpha);
+                    play(55, Number);
 
                 }
-                else if (b == 4)
+                else if (TargetRingNumber == 4)
                 {
                     Console.WriteLine("Geben sie die Zahl zwischen 1-20 ein welche sie treffen wollen!");
                     int Number = Int32.Parse(Console.ReadLine());
 
                     Radius = 85;
                 }
-                else if (b == 5)
+                else if (TargetRingNumber == 5)
                 {
                     Console.WriteLine("Geben sie die Zahl zwischen 1-20 ein welche sie treffen wollen!");
                     int Number = Int32.Parse(Console.ReadLine());
--- Test/Test_functionpolarcart/Program.cs
+++ Test/Test_functionpolarcart/Program.cs
@@ -54,7 +54,7 @@ namespace Test_functionpolarcart
                 Console.WriteLine(Convert.ToString(item));
             }
         }
-        private static int alpha(int number)
+        private static int TargetNumberToAlpha(int number)
         {
             int[] TargetNumber = new int[20] { 6, 13, 4, 18, 1, 20, 5, 12, 9, 14, 11, 8, 16, 7, 19, 3, 17, 2, 15, 10 };
             int[] middlealpha = new int[20] { 0, 18, 36, 54, 72, 90, 108, 126, 144, 162, 180, 198, 216, 234, 252, 270, 288, 306, 324, 342 };
@@ -69,19 +69,48 @@ namespace Test_functionpolarcart
             return 0;
         }
 
-        static void Main(string[] args)
+        private static int AlphaToTargetNumber(double alpha)
+        {
+            int[] TargetNumber = new int[20] { 6, 13, 4, 18, 1, 20, 5, 12, 9, 14, 11, 8, 16, 7, 19, 3, 17, 2, 15, 10 };
+            int[] MiddleAlpha = new int[20] { 9, 18, 36, 54, 72, 90, 108, 126, 144, 162, 180, 198, 216, 234, 252, 270, 288, 306, 324, 342 };
+
+            for (int i = 0; i < 19; i++)
+            {
+                if (alpha <= MiddleAlpha[i])
+                {
+                    return TargetNumber[i];
+                }
+            }
+            return 0;
+        }
+
+        private static double[] play(double Radius, int Number)
         {
             Random random = new Random();
             double RandomRadius = (double)random.NextDouble();
-            double RandomAlpha = (double)random.Next(0,10);
-            
-            double[] EndDistance = new double[2] {0,0};
+            double RandomAlpha = (double)random.Next(0, 360);
+
+            double[] EndDistance = new double[2] { 0, 0 };
+            double[] EndResult = new double[2] { 0, 0 };
             for (int i = 0; i < 2; i++)
             {
-                EndDistance[i] = PolarToCart(RandomRadius, RandomAlpha)[i] + PolarToCart(55, alpha(15))[i];
+                EndDistance[i] = PolarToCart(RandomRadius, RandomAlpha)[i] + PolarToCart(Radius, TargetNumberToAlpha(Number))[i];
             }
-            
-            ArrayToString(CartToPolar(EndDistance[0], EndDistance[1]));
+
+            for (int i = 0; i < 2; i++)
+            {
+                EndResult[i] = CartToPolar(EndDistance[0], EndDistance[1])[i];
+            }
+
+            Console.WriteLine(AlphaToTargetNumber(EndResult[1]));
+
+
+            return EndResult;
+        }
+
+        static void Main(string[] args)
+        {
+            play(55, 6);
             
         }
     }
